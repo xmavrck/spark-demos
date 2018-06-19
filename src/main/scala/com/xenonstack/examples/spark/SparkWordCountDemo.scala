44f07d3f3c837e14ea8a1e54e3057ac65cef0ae4
Navdeep Singh Gill <chandan@xenondigilabs.com>
2018-06-19 12:50:10 +0530

    word count demo

--- src/main/scala/com/xenonstack/examples/spark/SparkWordCountDemo.scala
+++ src/main/scala/com/xenonstack/examples/spark/SparkWordCountDemo.scala
@@ -6,11 +6,13 @@ object SparkWordCountDemo {
 
   def main(args: Array[String]): Unit = {
 
+    val BASE_PATH = "file:///home/dev/workspace/spark-demos"
+
     val conf = new SparkConf().setAppName("wordcount").setMaster("local[*]")
 
     val sc = new SparkContext(conf)
 
-    val textFile = sc.textFile("file:///home/dev/workspace/spark-demos/names.txt")
+    val textFile = sc.textFile(BASE_PATH + "/names.txt")
 
     val counts = textFile.flatMap(line => line.split(","))
 
@@ -18,7 +20,27 @@ object SparkWordCountDemo {
 
       .reduceByKey((accumulator, currentValue) => (accumulator + currentValue))
 
+    println("counts  "+counts.getNumPartitions)
+
+    //     runs a function on each object
     counts.foreach(println)
+
+    // return only 2 elements to avoid OOM errors
+    println(counts.take(2))
+
+
+    println(counts.first())
+
+    // to save the data in single partition
+    counts.coalesce(1).saveAsTextFile(BASE_PATH + "/results")
+
+
+    val countsRepartitioned = counts.repartition(10)
+
+    println("countsRepartitioned "+countsRepartitioned.getNumPartitions)
+
+    // to save the data in single partition
+    countsRepartitioned.saveAsTextFile(BASE_PATH + "/results_partitioned")
 
   }
 
